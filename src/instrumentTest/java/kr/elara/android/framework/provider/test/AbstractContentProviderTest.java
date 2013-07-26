c1ae7f724c1e690ef2295b388d53596936c0bd1e
yeongmin <yeongmin.kim@gmail.com>
2013-07-26 21:35:41 +0900

    Added test cases  - testQueryWithSelection  - testQueryAll  - added setup() to init stub items.

--- src/instrumentTest/java/kr/elara/android/framework/provider/test/AbstractContentProviderTest.java
+++ src/instrumentTest/java/kr/elara/android/framework/provider/test/AbstractContentProviderTest.java
@@ -2,16 +2,24 @@ package kr.elara.android.framework.provider.test;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.test.ProviderTestCase2;
 
 public class AbstractContentProviderTest extends ProviderTestCase2<MockContentProvider> {
 
+    private static final int STUB_ITEM_NUMBER = 10;
     private Uri mUri = Uri.parse(ContentResolver.SCHEME_CONTENT + "://" + MockContentProvider.AUTHORITY + "/" +
             ContentContract.Item.ITEMS);
 
     public AbstractContentProviderTest() {
         super(MockContentProvider.class, MockContentProvider.AUTHORITY);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        insertStubItems();
     }
 
     public void testInsertSingleItem() {
@@ -24,15 +32,40 @@ public class AbstractContentProviderTest extends ProviderTestCase2<MockContentPr
         Uri result = getMockContentResolver().insert(mUri, contentValues);
 
         Uri expected = Uri.parse(ContentResolver.SCHEME_CONTENT + "://" + MockContentProvider.AUTHORITY + "/" +
-                ContentContract.Item.ITEMS + "/" + 1);
+                ContentContract.Item.ITEMS + "/" + (STUB_ITEM_NUMBER + 1));
 
         assertEquals(expected, result);
     }
 
     public void testInsertMultipleItems() {
-        ContentValues[] values = new ContentValues[10];
+        int result = insertStubItems();
 
-        for (int i = 0; i < 10; ++i) {
+        assertEquals(STUB_ITEM_NUMBER, result);
+    }
+
+    public void testQueryWithSelection() {
+        Cursor cursor = getMockContentResolver().query(mUri, new String[]{Item.NUMBER}, Item.NUMBER + " = ?",
+                new String[]{"3"}, null);
+
+        assertEquals(1, cursor.getCount());
+        assertEquals(Item.NUMBER, cursor.getColumnNames()[0]);
+
+        cursor.moveToFirst();
+        assertEquals("3", cursor.getString(0));
+    }
+
+    public void testQueryAll() {
+        Cursor cursor = getMockContentResolver().query(mUri, new String[]{Item.TITLE}, null, null, null, null);
+        int count = cursor.getCount();
+
+        assertEquals(STUB_ITEM_NUMBER, count);
+    }
+
+
+    private int insertStubItems() {
+        ContentValues[] values = new ContentValues[STUB_ITEM_NUMBER];
+
+        for (int i = 0; i < STUB_ITEM_NUMBER; ++i) {
             ContentValues contentValues = new ContentValues();
 
             contentValues.put(Item.TITLE, "test " + i);
@@ -42,8 +75,6 @@ public class AbstractContentProviderTest extends ProviderTestCase2<MockContentPr
             values[i] = contentValues;
         }
 
-        int result = getMockContentResolver().bulkInsert(mUri, values);
-
-        assertEquals(10, result);
+        return getMockContentResolver().bulkInsert(mUri, values);
     }
 }
